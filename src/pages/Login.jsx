c9270e22fa715ca8b89e61aa743c33d364e2a279
sdryasir <sdr.yasir@gmail.com>
2022-06-07 03:49:47 -0700

    basic form handling and form validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [emailError, setEmailError] = useState('')
+    const [emailPassword, setPasswordError] = useState('')
+
+    const handleEmailChange = (event)=>{
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordChange = (event)=>{
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+
+        if(!email){
+            setEmailError('Pleae provide Email');
+            return
+        }else{
+            setEmailError('')
+        }
+        if(!password){
+            setPasswordError('Please provide password');
+            return
+        }else{
+            setPasswordError('')
+        }
+        console.log(email, password)
+    }
+
   return (
-    <form>
-        <div class="row mb-3">
-            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3"/>
+    <>
+        <form onSubmit={handleSubmit}>
+        <div className="row mb-3">
+            <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+            <div className="col-sm-10">
+                <input type="email" onChange={handleEmailChange} className="form-control" id="email"/>
+                <p style={{color:'red'}}>{emailError}</p>
             </div>
         </div>
-        <div class="row mb-3">
-            <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-            <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3"/>
+        <div className="row mb-3">
+            <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+            <div className="col-sm-10">
+                <input type="password" onChange={handlePasswordChange} className="form-control" id="password"/>
+                <p style={{color:'red'}}>{emailPassword}</p>
             </div>
         </div>
-        
-        <div class="row mb-3">
-            <div class="col-sm-10 offset-sm-2">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck1"/>
-                <label class="form-check-label" for="gridCheck1">
-                Example checkbox
-                </label>
-            </div>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" className="btn btn-primary">Sign in</button>
     </form>
+    <div></div>
+    </>
   )
 }
 
--- src/products.js
+++ src/products.js
@@ -4,7 +4,8 @@ export const products = [
       title:"Crispy Burger",
       description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
       price:250,
-      stock:20
+      stock:20,
+      image:'./assets/img/product1.png'
     },
     {
       id:2,
